e77baaad59f4c8ac1801deb9ac05e3b2f828e75d
rdprassy <rdprassy@gmail.com>
2020-10-04 22:03:40 +0530

    added the react component and its rendering for some basic text

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,10 +5,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-
-
-
 // Create a react component
+const App = function(){  // =() => {
+
+    return <div> Hi there!</div>;
+};
 
 
 // Take the react component and show it on the screen
+ReactDOM.render(<App/>, document.querySelector('#root'));
+//calling the app react component and asking it to be shown in the root div which is present inside the
+//public folder with the filename index.html
