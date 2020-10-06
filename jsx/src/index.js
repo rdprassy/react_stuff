f786cf8202704e9b3f557f43820ed137f2c9c477
rdprassy <rdprassy@gmail.com>
2020-10-06 22:53:04 +0530

    added a input text in the js file just to see how jsx takes styles

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,9 +6,18 @@ import ReactDOM from 'react-dom';
 
 
 // Create a react component
-const App = function(){  // =() => {
+const App  =() => {
 
-    return <div> Hi there!</div>;
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type = "text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                Submit
+            </button>
+        </div>);
 };
 
 
