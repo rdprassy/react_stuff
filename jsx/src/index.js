db102258a9f480f2796dc41608fe28f99a810059
rdprassy <rdprassy@gmail.com>
2020-10-06 23:05:06 +0530

    added a js variable and called it inside the react component creation jsx

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,9 +4,13 @@ import React from 'react';
 // import <varible_stored_library' form 'library'
 import ReactDOM from 'react-dom';
 
-
+function getButtonText() {
+    return "from_the_function"
+}
 // Create a react component
 const App  =() => {
+
+    const buttonText = 'Click Me!';
 
     return (
         <div>
@@ -15,7 +19,7 @@ const App  =() => {
             </label>
             <input id="name" type = "text" />
             <button style={{backgroundColor: 'blue', color: 'white'}}>
-                Submit
+                Submit {buttonText} {getButtonText()}
             </button>
         </div>);
 };
