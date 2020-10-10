c4f85d46181162679b8ded1e4d00f09730d9f79e
rdprassy <rdprassy@gmail.com>
2020-10-10 22:51:35 +0530

    created a constructor and initialised the state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,6 +4,16 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
+
+  // not specific to react.
+    constructor(props) {
+        super(props);
+        // try to initialise your state
+
+        // let it be initialised with null
+        this.state = {lat: null};
+    }
+    // react requirement for class components.
     render() {
 
         window.navigator.geolocation.getCurrentPosition(
