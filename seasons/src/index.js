1bf9f844f4d022579ca3ff220dfea98981cf9362
rdprassy <rdprassy@gmail.com>
2020-10-10 23:02:10 +0530

    Worked on reshuffling things of get postion, and setting up the state once the call is made.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,14 +4,23 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-
+  // React. Component provides all these constructor, render and setState.
   // not specific to react.
     constructor(props) {
         super(props);
         // try to initialise your state
 
         // let it be initialised with null
+        // only direct initialisation with this. on states
         this.state = {lat: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> {console.log(position); this.setState({ lat: position.coords.latitude})},
+            (err) => console.log(err)
+        );
+
+
+
     }
     // react requirement for class components.
     render() {
@@ -21,7 +30,7 @@ class App extends React.Component {
             (err) => console.log(err)
         );
 
-        return <div> Latitude:  </div>
+        return <div> Latitude: {this.state.lat} </div>
     }
 }
 
