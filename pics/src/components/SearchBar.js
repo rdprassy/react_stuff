5ee5cc1dd2f12169580246863accdc3898ea3368
rdprassy <rdprassy@gmail.com>
2020-10-11 19:23:47 +0530

    added some css to the input text to look ok

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,10 +5,13 @@ import React from "react";
 class SearchBar extends React.Component {
 
     render() {
-        return (<div>
-        <form>
-            <input type = "text"/>
+        return (<div className= "ui segment">
+        <form className= "ui form">
+            <div className= "field">
 
+        <label> Image Search </label>
+            <input type = "text"/>
+            </div>
         </form>
                 </div>
 
