f062f3e35273e57bda75b55d1aa7783c18a9c610
rdprassy <rdprassy@gmail.com>
2020-10-11 19:09:14 +0530

    Introduced searchbar component with a basic input text

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Class based component inorder to handle a state
+
+class SearchBar extends React.Component {
+
+    render() {
+        return (<div>
+        <form>
+            <input type = "text"/>
+
+        </form>
+                </div>
+
+        );
+    }
+}
+
+export default SearchBar;
