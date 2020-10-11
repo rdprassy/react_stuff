9539616dbd7567c45a083f3344322fd78e36ada6
rdprassy <rdprassy@gmail.com>
2020-10-12 00:12:22 +0530

    correctly rendering this things removing errors

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -16,21 +16,29 @@ class SearchBar extends React.Component {
 
     }
 
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         event.preventDefault()
-      //  console.log(this.state.term) returns error but why
+      console.log(this.state.term) //returns error but why
     }
 
 
     render() {
         return (<div className= "ui segment">
         <form className= "ui form" onSubmit={this.onFormSubmit}>
+
+            {/*other way of solving the this problem*/}
+            {/*<form onSubmit={event => this.onFormSubmit(event)}*/}
+            
+
             <div className= "field">
 
         <label> Image Search </label>
             <input type = "text" value={this.state.term} onClick={this.onInputClick} onChange= {(e) => this.setState({term: e.target.value.toUpperCase()})} />
             {/*// can also be called*/}
             {/*    // onChange= {(e) => console.log(e.target.value)}*/}
+
+
+
             </div>
         </form>
                 </div>
