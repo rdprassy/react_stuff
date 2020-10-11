7282f7831e4053d60291f6e1bcf250a3794e3cbb
rdprassy <rdprassy@gmail.com>
2020-10-11 19:37:06 +0530

    added some basic event handler onChange for the textbox

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,13 +4,24 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
+    onInputChange(event) {
+        console.log(event.target.value);
+
+    }
+
+    onInputClick(event) {
+        console.log('input is clicked');
+
+    }
+
     render() {
         return (<div className= "ui segment">
         <form className= "ui form">
             <div className= "field">
 
         <label> Image Search </label>
-            <input type = "text"/>
+            <input type = "text" onClick={this.onInputClick} onChange= {this.onInputChange}/>
+
             </div>
         </form>
                 </div>
