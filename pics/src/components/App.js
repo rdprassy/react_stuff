eb8368655ad848599084f16ba6f0d05792d988ae
rdprassy <rdprassy@gmail.com>
2020-10-18 16:57:27 +0530

    added code to call an api using the axios way of calling rest api calls

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -12,7 +12,27 @@ class App extends React.Component{
     onSearchSubmit(term){
         console.log(term);
 
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+           headers: {
+               Authorization: 'Client-ID hZNxX1BSxDzpmkUkFSG-tNgQoaHQO-IIvqeKDZEsM-o'
+           }
+        }).then((response) => {
+            console.log(response.data.results)
+        })
+
         // write code for
+
+      this.diffAPICall()
+
+        // another way of calling is async and wait
+
+    }
+
+    diffAPICall() {
+
+        console.log("inside the diff api call ")
+
     }
 
     render() {
