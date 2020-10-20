171ad0af987aa984e737a8fc13d9107bde7268b4
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-20 19:07:39 +0530

    converting a list of json data into returnable html doms

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const ImageList = (props) => {
 
+    // const images = props.images.map((image) => <img alt={image.description} key={image.id} src={image.urls.regular} />)
+
+
+    // shorter way
+
+    const images = props.images.map(({description, id, urls }) => <img alt={description} key={id} src={urls.regular} />)
+
     console.log(props.images)
-    return <div>ImageList</div>
+    return <div>{images}</div>
 };
 
 
