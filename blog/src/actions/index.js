652828b930c92c8d7eeaeecdcb4b811ff75f2a23
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-26 19:52:06 +0530

    called apis with axiom using asynch await but however problems due to action creators creating actions due to asynch await

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPosts = async() => {
+    const response = await jsonPlaceholder.get('/posts');
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
+
+    // as we use asynch await on success it returns request object which isn't a plain js object
+    //By the time our action gets to a reducer, we won't have fetched our data.
 };
