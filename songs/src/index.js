d2d279689aef177084f4bb55a9cde01b7d8d0d03
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-26 07:29:18 +0530

    wiring up the provider in main index file helping creating a store

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from './reducers'
 
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={createStore(reducers)}>
     <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
