a2677b7042747bf4019f1a9e948a9eb535b77148
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-26 20:31:28 +0530

    calling aynch apis using redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,13 +1,19 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async() => {
-    const response = await jsonPlaceholder.get('/posts');
-
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
+export const fetchPosts = () =>
+    async  dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch({type: 'FETCH_POSTS', payload: response })
     };
+
+
+
 
     // as we use asynch await on success it returns request object which isn't a plain js object
     //By the time our action gets to a reducer, we won't have fetched our data.
-};
+    // to solve such issues we use redux-thunk middle ware helps action creators can return action objects or return functions
+
+    // redux-thunk allows you to manually create a dispatch unlike connect. (await for the response obj is ready from api
+
+    // gives unlimited power on react store with getState and dispatch function over arguments
+
