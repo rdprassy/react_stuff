4b308586d7073739226012cca03efcafb8bbc90e
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-26 07:58:52 +0530

    rendering the songs list on the screen

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -2,13 +2,32 @@ import React from 'react';
 
 import {connect } from 'react-redux'; // react component
 class SongList extends React.Component {
+
+    renderList(){
+        //return blob of songlist
+
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key = {song.title}>
+                    <div className="right floated content">
+                        <button className = "ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className = "content">{song.title}</div>
+                </div>
+            )
+        })
+    }
+
+
     render(){
 
         //this.props get data from redux store in to the component
 
-        console.log(this.props)
+        console.log(this.props.songs)
 
-        return <div> SongList</div>
+        return <div className="ui divided list"> {this.renderList()}</div>
     }
 }
 
