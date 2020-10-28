b28b3763834ea738b182a7a5930bed10720e28c5
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-28 21:01:22 +0530

    call the posts axios api and fetch results using axios, action type using connect and reduce it to send the required information

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -11,10 +11,18 @@ class PostList extends React.Component {
 
 
     render() {
+        console.log(this.props.posts);
         return <div> Post List</div>
     }
+
+    //get value from redux state to react
 
 
 }
 
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = state => {
+    return { posts: state.posts }
+}
+
+
+export default connect(mapStateToProps, {fetchPosts})(PostList);
