d87f53f108f8bf793fb5b7058f428befbd9ba8ce
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-28 20:44:32 +0530

    good practise of having switch actions in reducers

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,3 +1,18 @@
-export default () => {
-   return 123;
-}
+export default (state = [], action) => {
+   // if(action.type === 'FETCH_POSTS') {
+   //    return action.payload;
+   // }
+
+   //try to use switch
+
+   switch (action.type){
+      case 'FETCH_POSTS':
+         return action.payload;
+      default:
+         return state;
+   }
+
+   // return state;
+};
+
+
