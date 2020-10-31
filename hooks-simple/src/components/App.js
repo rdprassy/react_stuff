6959583c85a4f5437aa5cacc09c33a7960cdcf8b
Devi Prasad Personal <rdprassy@gmail.com>
2020-10-31 17:03:25 +0530

    added the usage of useState

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React , {useState} from 'react';
 
-class App extends React.Component {
+const App = () => {
 
-    state = {resource: "posts"};
 
-    render() {
+    const [resource, setResource] = useState('posts');
 
-        return <div>
+
+        return (
             <div>
-                <button onClick={()=> this.setState({resource: 'posts'})} > Posts</button>
-                <button onClick={()=> this.setState({resource:'todos'})}> Todos</button>
+            <div>
+                <button onClick={()=> setResource('posts')} > Posts</button>
+                <button onClick={()=> setResource('todos')}> Todos</button>
             </div>
-            {this.state.resource}
-        </div>;
+            {resource}
+        </div>
+        );
     }
-}
+
 
 
 export default App;
