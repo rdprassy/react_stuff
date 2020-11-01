a48b4f26cb9bb30f97dc28ff879deb4c9d4bcbb1
Devi Prasad Personal <rdprassy@gmail.com>
2020-11-01 21:01:28 +0530

    created separeate components of stream to display using react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 const PageOne =() =>{
-    return <div>PageOne</div>
+    return <div>PageOne
+    <br/>
+     <Link to="/pagetwo"> Navigate to Page Two </Link>
+    </div>
 }
 
 const PageTwo =() => {
     return <div>PageTwo
     <button> Click me</button>
+
+        <br/>
+        <Link to="/"> Navigate to Page One </Link>
     </div>
 }
 
