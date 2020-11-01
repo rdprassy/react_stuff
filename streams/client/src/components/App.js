dbe42132ec01048869a7e9ee229d4308b0f6e318
Devi Prasad Personal <rdprassy@gmail.com>
2020-11-01 21:27:12 +0530

    added all routes to stream and a common header

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-const PageOne =() =>{
-    return <div>PageOne
-    <br/>
-     <Link to="/pagetwo"> Navigate to Page Two </Link>
-    </div>
-}
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamCreate";
+import StreamList from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamCreate";
+import Header from "./Header";
 
-const PageTwo =() => {
-    return <div>PageTwo
-    <button> Click me</button>
 
-        <br/>
-        <Link to="/"> Navigate to Page One </Link>
-    </div>
-}
 
 
 const App = () => {
     return (
-        <div>App
+        <div>
+
          <BrowserRouter>
              <div>
-                 <Route path="/" exact component={PageOne}/>
-                 <Route path="/pageTwo" exact component={PageTwo}/>
+                 <Header/>
+                 <Route path="/" exact component={StreamList}/>
+                 <Route path="/streams/new" exact component={StreamCreate}/>
+                 <Route path="/streams/edit" exact component={StreamEdit}/>
+                 <Route path="/streams/delete" exact component={StreamDelete}/>
+                 <Route path="/streams/show" exact component={StreamShow}/>
              </div>
 
 
