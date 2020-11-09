997bc0ce1958addb9d53402af9a549f1c6dbac30
Devi Prasad Personal <rdprassy@gmail.com>
2020-11-09 09:21:15 +0530

    using consumer to fetch values from context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,14 +3,15 @@ import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
 
-    static contextType = LanguageContext;
+    static contextType = LanguageContext; // only required for this.context
 
     // equal to Button.contextType= LanguageContext
 
     render(){
         console.log(this.context)
         const text = this.context === 'english'? 'Submit': 'Voorleggen';
-        return <button className = "ui button primary"> {text} </button>
+        // return (<button className = "ui button primary"> {text} </button>);
+        return (<button className = "ui button primary"> <LanguageContext.Consumer>{(value)=> value === 'english'? 'Submit': 'Voorleggen' }</LanguageContext.Consumer> </button>);
     }
 }
 
